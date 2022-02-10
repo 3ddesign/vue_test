99095ac481b153c8961deb3e569cb863bcdf4b10
Dmitry <3ddesign@bk.ru>
2022-02-10 09:47:21 +0200

    add about section

---
+++ src/components/About.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="about">
+    <div class="container">
+      <h2>About me</h2>
+      <div class="pfblock-line"></div>
+      <img src="../assets/images/Dmitry.jpg" alt="Dmitry Scherbakov">
+      <div class="pfblock-subtitle">
+        Hi! My name is Dmitry. I'm a frontend developer.
+        <br>I use Angular as the main framework, also I have an experience of creating MEAN stack projects and
+          mobile applications. I am passionate about development with JavaScript and have an
+          aspiration to keep improving in it.
+      </div>
+      <br>
+      <table class="table table-hover text-center table-striped form-group wow fadeInUp animated">
+        <caption>Technical expertise:</caption>
+        <tbody>
+          <tr>
+            <td>JavaScript</td>
+          </tr>
+          <tr>
+            <td>HTML, CSS</td>
+          </tr>
+          <tr>
+            <td>Angular 2+, React</td>
+          </tr>
+          <tr>
+            <td>TypeScript, RxJS, NgRx</td>
+          </tr>
+          <tr>
+            <td>Experience with Node.js</td>
+          </tr>
+          <tr>
+            <td>MEAN stack projects development</td>
+          </tr>
+          <tr>
+            <td>Working with Agile development processes</td>
+          </tr>
+          <tr>
+            <td>Adaptive layout using different preprocessors and frameworks</td>
+          </tr>
+          <tr>
+            <td>Experience in creating applications with Ionic, NativeScript and PWA</td>
+          </tr>
+          <tr>
+            <td>
+              Extensive experience of network and system administration: configuring
+              servers, deployment and maintenance of various network solutions, etc.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
